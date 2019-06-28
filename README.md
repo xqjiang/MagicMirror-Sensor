a9138ed219e1b6d5667e286a7d95ca3809f93756
xqjiang <yangxingjiang@gmail.com>
2019-06-28 14:40:37 +0800

    GooglePhotos Folder - mainfolder

---
+++ README.md
@@ -0,0 +1,87 @@
+# MMM-GooglePhotos
+Display your photos from album of Google Photos on MagicMirror
+
+### Screenshot
+[[PlaceHolder]]
+
+### Installation
+
+1. Install Module
+```sh
+git clone https://github.com/eouia/MMM-GooglePhotos.git
+cd MMM-GooglePhotos
+npm install
+```
+
+If you doubt errors are caused by different `node.JS` version, you can try to compile it by yourself.
+In that case, you need to rebuild some binaries to match with Electron version.
+```sh
+cd ~/MagicMirror/modules/MMM-GooglePhotos
+npm install --save-dev electron-rebuild
+./node_modules/.bin/electron-rebuild   # It could takes dozens sec.
+```
+
+### Get `Auth` and `AlbumId`
+1. Go to [Google API Console](https://console.developers.google.com/)
+2. From the menu bar, select a project or create a new project.
+3. To open the Google API Library, from the Navigation menu, select `APIs & Services > Library`.
+4. Search for "Google Photos Library API". Select the correct result and click Enable. (You may need to enable "Google Plus" also.)
+5. Then, from the menu, select `APIs & Services > Credentials`.
+6. On the Credentials page, click `Create Credentials > OAuth client ID`.
+7. Select your Application type as `Other` and submit. (Before or After that, you might be asked for making consent screen. do that.)
+8. Then, you can download your credential json file from list. Downloaded file name would be `client_secret_xxxx...xxx.json`. rename it as `credentials.json` and save it to your `MMM-GooglePhotos` directory.
+9. Now, open your termial(not via SSH, directly in your RPI).
+```shell
+cd ~/MagicMirror/modules/MMM-GooglePhotos
+node auth_and_test.js
+```
+10. At first execution, It will open a browser and will ask you to login google account and to consent your allowance.
+11. After consent, some code (`4/ABCD1234xxxx...`) will be appeared. copy it and return to your terminal. paste it for answer of prompt in console.
+12. Now you can get list of your Google Photos albums. like these;
+```
+<ALBUM_NAME> : <ALBUM_ID>
+travel to paris : AGj1epU5VMNoBGK9GDX3k_zDQaPT16Fe56o0N93eN6aXn-f21M98
+...
+```
+13. Remember the id of album to show.
+14. now set your `config.js`
+
+
+### Configuration
+```javascript
+{
+  module: "MMM-GooglePhotos",
+  position: "top_right",
+  config: {
+    albumId: ["YOUR_GOOGLE_PHOTOS_ALBUM_ID","NEXT_GOOGLE_PHOTOS_ALBUM_ID"], // your album id(s) from result of `auth_and_test.js`
+    refreshInterval: 1000*60,  
+    scanInterval: 1000*60*10, // too many scans might cause API quota limit also.
+    //note(2018-07-29). It is some weird. API documents said temporal image url would live for 1 hour, but it might be broken shorter. So, per 10 min scanning could prevent dead url.
+
+    sort: "time", //'time', 'reverse', 'random'
+    showWidth: "800px", // how large the photo will be shown as. (e.g;'100%' for fullscreen)
+    showHeight: "600px",
+    originalWidthPx: 800, // original size of loaded image. (related with image quality)
+    originalHeightPx: 600, // Bigger size gives you better quality, but can give you network burden.
+    mode: "hybrid", // "cover" or "contain" (https://www.w3schools.com/cssref/css3_pr_background-size.asp)
+    //ADDED. "hybrid" : if you set as "hybrid" it will change "cover" and "contain" automatically by aspect ratio.
+  }
+},
+```
+
+### Last Tested;
+- MagicMirror : v2.4.1
+- node.js : 8.11.3 & 10.x
+
+
+### Update
+#### [2018-12-29]
+- Configure one or more albumIds
+
+#### [2018-07-29]
+- Fix the issue of dead url.
+(It seems also the problem of API. documents said temporal url would live for 1 hour, but it might be broken shorter.)
+
+#### [2018-07-22]
+- Fix the issue of photos shared from others.
+(I think it was the problem of API, because `mediaItem:search` can access shared photos but `mediaItem/Id` cannot. That is out of sense.)
---
+++ node_helper.js
@@ -0,0 +1,223 @@
+
+//
+// Module : MMM-Hotword
+//
+
+'use strict'
+
+const path = require("path")
+const request = require('request')
+const Auth = require('./auth.js')
+
+
+var NodeHelper = require("node_helper")
+
+module.exports = NodeHelper.create({
+  start: function() {
+    console.log(this.name + " started")
+    this.config = {}
+    this.items = []
+    this.tempItems = []
+    this.accessToken = ""
+    this.authConfig = {}
+    this.started = false
+    this.index = 0
+    //this.auth = null
+  },
+
+  initializeAfterLoading: function (config) {
+    if (config.scanInterval > 1000 * 60 * 10) {
+      config.scanInterval = 1000 * 60 * 10 // By using baseUrl from :search directly, there needs to maintain scanInterval under 1hour because risk of expiration of baseUrl.
+    }
+    this.config = config
+    console.log(this.name + " initialized after loading.")
+    this.authConfig = {
+      keyFilePath : path.resolve(__dirname, "credentials.json"),
+      savedTokensPath : path.resolve(__dirname, "token.json"),
+      scope: "https://www.googleapis.com/auth/photoslibrary.readonly"
+    }
+    this.scanPhotos()
+    this.scanTimer = setInterval(()=>{
+      this.scanPhotos()
+    },this.config.scanInterval)
+  },
+
+  scanPhotos: function() {
+    this.tempItems = []
+    var auth = new Auth(this.authConfig)
+    auth.on('ready', (client) => {
+      this.accessToken = client.credentials.access_token
+      this.getPhotos()
+    })
+
+  },
+
+  socketNotificationReceived: function (notification, payload) {
+    switch(notification) {
+      case 'INIT':
+        this.initializeAfterLoading(payload)
+        this.sendSocketNotification('INITIALIZED')
+        break
+    }
+  },
+
+  getPhotos: function(){
+    var albums = this.config.albumId
+    if (!(albums instanceof Array)) {
+      albums = [this.config.albumId]
+    }
+    for (var i in albums) {
+      this.getPhotosByAlbumId(albums[i])
+    }
+  },
+
+  getPhotosByAlbumId: function(albumId) {
+    const options = {
+      url: "https://photoslibrary.googleapis.com/v1/mediaItems:search",
+      method: "POST",
+      form: {
+        'albumId': albumId,
+        'pageSize': 50,
+        'pageToken': "",
+      },
+      json: true,
+      auth: {
+        "bearer": this.accessToken
+      }
+    }
+    var self = this
+    function getItems(options) {
+      request.post(options, (err, res, body) => {
+        if (err) {
+          console.log("Error:", err)
+          return
+        }
+        var mediaItems = body.mediaItems
+        var found = 0
+        if (mediaItems) {
+          found = mediaItems.length
+        }
+        for (var i in mediaItems) {
+          //console.log("scanned:", mediaItems[i])
+          if ("photo" in mediaItems[i].mediaMetadata) {
+            var item = {
+              "id": mediaItems[i].id,
+              "creationTime": Date.parse(mediaItems[i].mediaMetadata.creationTime),
+              "baseUrl": mediaItems[i].baseUrl,
+              "width": mediaItems[i].mediaMetadata.width,
+              "height": mediaItems[i].mediaMetadata.height
+            }
+            self.tempItems.push(item)
+          }
+        }
+        if (found > 0 && body.nextPageToken) {
+          options.form.pageToken = body.nextPageToken
+          getItems(options)
+        } else {
+          self.finishedScan()
+        }
+      })
+    }
+    getItems(options)
+  },
+
+  finishedScan: function() {
+    switch(this.config.sort) {
+      case "time":
+        this.tempItems.sort((a, b)=>{
+          return b.creationTime - a.creationTime
+        })
+        break
+      case "reverse":
+        this.tempItems.sort((a, b)=>{
+          return a.creationTime - b.creationTime
+        })
+        break
+      case "random":
+        var currentIndex = this.tempItems.length, temporaryValue, randomIndex
+        while (0 !== currentIndex) {
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+          temporaryValue = this.tempItems[currentIndex];
+          this.tempItems[currentIndex] = this.tempItems[randomIndex];
+          this.tempItems[randomIndex] = temporaryValue;
+        }
+        break
+    }
+    console.log("[GPHOTO] Scan finished :", this.tempItems.length)
+    //this.sendSocketNotification("IMAGE_LIST", this.items)
+    this.items = this.tempItems
+    if (this.started == false) {
+      this.started = true
+      this.broadcast()
+    }
+  },
+
+/* Changed for shared Image.
+ * Very weird API. I don't know why "mediaItems:search" can access shared photos but "mediaItems/[photoID]" cannot.
+ * anyway, here will be trick.
+  getPhoto: function() {
+    var photoId = this.items[this.index].id
+    const options = {
+      url: "https://photoslibrary.googleapis.com/v1/mediaItems/" + photoId,
+      method: "GET",
+      json: true,
+      auth: {
+        "bearer": this.accessToken
+      }
+    }
+    request.get(options, (err, res, body) => {
+      if (err) {
+        console.log("Error:", err)
+        return
+      }
+      this.index++
+      if (this.index >= this.items.length) {
+        this.index = 0
+      }
+      console.log("body", body)
+      var payload = {
+        "id":body.id,
+        "url":body.baseUrl + "=w" + this.config.originalHeightPx + "-h" + this.config.originalHeightPx,
+        "time": Date.parse(body.mediaMetadata.creationTime),
+        //more metadata will be provided later.
+      }
+      this.sendSocketNotification("NEW_IMAGE", payload)
+    })
+  },
+*/
+  getPhoto: function() {
+    if (this.items.length <= 0) {
+      console.log("There is no scanned photo currently.")
+      return
+    }
+    var photo = null
+    if (typeof this.items[this.index] !== "undefined") {
+      photo = this.items[this.index]
+    } else {
+      photo = this.items[0]
+      this.index = 0
+    }
+    //console.log("photo", this.index, this.items.id)
+    this.index++
+
+    var payload = {
+      "id":photo.id,
+      "url":photo.baseUrl + "=w" + this.config.originalWidthPx + "-h" + this.config.originalHeightPx,
+      "time": Date.parse(photo.creationTime),
+      "width": photo.width,
+      "height": photo.height
+    }
+    //console.log("image", photo.baseUrl)
+    this.sendSocketNotification("NEW_IMAGE", payload)
+  },
+
+
+
+  broadcast: function() {
+    this.getPhoto()
+    var timer = setTimeout(()=>{
+      this.broadcast()
+    }, this.config.refreshInterval)
+  }
+})
